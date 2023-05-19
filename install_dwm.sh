300dfa697a21716b3da7301ea89ed1f23f41c6c0
J.E. Alcalá <jaime.atemores@alumnos.udg.mx>
2023-05-19 09:13:39 -0600

    Update install_dwm.sh

--- install_dwm.sh
+++ install_dwm.sh
@@ -118,6 +118,7 @@ RUNZSH=no KEEP_ZSHRC=yes sh -c "$(curl -fsSL https://raw.githubusercontent.com/o
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+git clone https://github.com/jirutka/zsh-shift-select.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-shift-select
 starship preset pastel-powerline -o ~/.config/starship.toml
 
 # Install dwm
